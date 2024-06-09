49a8160bed97a9865289108934f71626d8b055a7
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2024-06-09 21:10:58 +0200

    hammer

--- lib/cli.ex
+++ lib/cli.ex
@@ -31,20 +31,10 @@ defmodule Blitzy.CLI do
     end
   end
 
-  defp do_requests(n_requests, url, nodes) do
+  defp do_requests(n_requests, url, _) do
     Logger.info "Pummelling #{url} with #{n_requests} requests"
-
-    total_nodes  = Enum.count(nodes)
-    req_per_node = div(n_requests, total_nodes)
-
-    nodes
-    |> Enum.flat_map(fn node ->
-         1..req_per_node |> Enum.map(fn _ ->
-           Task.Supervisor.async({TasksSupervisor, node}, Blitzy.Worker, :start, [url])
-         end)
-       end)
-    |> Enum.map(&Task.await(&1, :infinity))
-    |> parse_results
+    Interval.start_link(%{req_per_node: n_requests, url: url})
+    Process.sleep(20_000)
   end
 
   defp do_help() do
@@ -59,44 +49,6 @@ defmodule Blitzy.CLI do
     ./blitzy -n 100 http://www.bieberfever.com
     """
     System.halt(0)
-  end
-
-  defp parse_results(results) do
-    {successes, _failures} =
-      results
-        |> Enum.partition(fn x ->
-             case x do
-               {:ok, _} -> true
-               _        -> false
-           end
-         end)
-
-    total_workers = Enum.count(results)
-    total_success = Enum.count(successes)
-    total_failure = total_workers - total_success
-
-    data = successes |> Enum.map(fn {:ok, time} -> time end)
-    average_time  = average(data)
-    longest_time  = Enum.max(data)
-    shortest_time = Enum.min(data)
-
-    IO.puts """
-    Total workers    : #{total_workers}
-    Successful reqs  : #{total_success}
-    Failed reqs      : #{total_failure}
-    Average (msecs)  : #{average_time}
-    Longest (msecs)  : #{longest_time}
-    Shortest (msecs) : #{shortest_time}
-    """
-  end
-
-  defp average(list) do
-    sum = Enum.sum(list)
-    if sum > 0 do
-      sum / Enum.count(list)
-    else
-      0
-    end
   end
 
 end
---
+++ lib/interval.ex
@@ -0,0 +1,89 @@
+defmodule Interval do
+  use GenServer
+
+  alias Blitzy.TasksSupervisor
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts)
+  end
+
+  @impl GenServer
+  def init(state) do
+    state =
+      state
+      |> Map.put_new(:interval, 5_000)
+      |> Map.put_new(:schedule, nil)
+
+    # optional; to be called if the first message
+    #           from outside expects the periodic
+    #           function to be called at least once
+    {:noreply, state} = handle_info(:work, state)
+    {:ok, state}
+  end
+
+  @impl GenServer
+  def handle_info(:work, state) do
+    # process periodic event
+
+    # optional; to allow calling it from outside without
+    #           setting many subsequent timers
+    # if is_reference(state.schedule),
+    #   do: Process.cancel_timer(state.schedule)
+    IO.inspect state
+    req_per_node = Map.get(state, :req_per_node)
+    url = Map.get(state, :url)
+    [node()|Node.list]
+    |> Enum.flat_map(fn node ->
+         1..req_per_node |> Enum.map(fn _ ->
+           Task.Supervisor.async({TasksSupervisor, node}, Blitzy.Worker, :start, [url])
+         end)
+       end)
+    |> Enum.map(&Task.await(&1, :infinity))
+    |> parse_results
+
+    {:noreply, Map.put(state, :schedule, schedule_work(state.interval))}
+  end
+
+  @spec schedule_work(interval :: non_neg_integer()) :: :error | reference()
+  defp schedule_work(interval) when is_integer(interval) and interval > 0 do
+     Process.send_after(self(), :work, interval)
+  end
+
+  def parse_results(results) do
+    {successes, _failures} =
+      results
+        |> Enum.partition(fn x ->
+             case x do
+               {:ok, _} -> true
+               _        -> false
+           end
+         end)
+
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
+
+    data = successes |> Enum.map(fn {:ok, time} -> time end)
+    average_time  = average(data)
+    longest_time  = Enum.max(data)
+    shortest_time = Enum.min(data)
+
+    IO.puts """
+    Total workers    : #{total_workers}
+    Successful reqs  : #{total_success}
+    Failed reqs      : #{total_failure}
+    Average (msecs)  : #{average_time}
+    Longest (msecs)  : #{longest_time}
+    Shortest (msecs) : #{shortest_time}
+    """
+  end
+
+  def average(list) do
+    sum = Enum.sum(list)
+    if sum > 0 do
+      sum / Enum.count(list)
+    else
+      0
+    end
+  end
+
+end
