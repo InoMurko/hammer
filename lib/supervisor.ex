4f87c9f0bf85a9441ec3317fad5fc922663943fe
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2024-06-09 21:35:11 +0200

    hammer

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -7,7 +7,7 @@ defmodule Blitzy.Supervisor do
 
   def init(:ok) do
     children = [
-      :hackney_pool.child_spec(:rpc_pool,  [timeout: 15000, max_connections: 500]),
+      :hackney_pool.child_spec(:rpc_pool,  [timeout: 15000, max_connections: 600]),
       supervisor(Task.Supervisor, [[name: Blitzy.TasksSupervisor]])
     ]
 
